221791dc6d4346ed1b5046cc8ac99f78676f56b9
stv <stv@stanford.edu>
2014-08-01 00:11:31 -0700

    Refactor image modal xmodule
    Notable Changes:
    - Cache jQuery lookups
        - Most of these jQuery selectors are used multiple times, so caching
          them locally provides a performance gain. It has the added
          side-effect of reducing the risk of typos (see: 'action-close' vs.
          'action-remove') [0]
    - Remove onHover events
        - Instead of using hover events to indicate which button is being
          clicked, we now use `event.target`.
    - Remove unneeded conditional statements
        - This provides simpler, less-indented code with minor performance
          gains (see: onHover events).
    - Rework template markup
        - `<i>` tags are now siblings of `<span>` tags, not children.
    - Update style
        - JavaScript now uses four-space indents, per the style guide [1]
    - Shorten long lines in template
    
    [0] https://github.com/edx/edx-platform/blob/5e67612e9b0234e6d2b1a02b81ef2bd1bd95579b/common/lib/xmodule/xmodule/js/src/html/imageModal.js#L63
    [1] https://github.com/edx/edx-platform/wiki/Javascript-standards-for-the-edx-platform#code-style

--- common/lib/xmodule/xmodule/js/src/html/imageModal.js
+++ common/lib/xmodule/xmodule/js/src/html/imageModal.js
@@ -1,113 +1,108 @@
-$(document).on('load_full-screen-image', 'a.modal-content IMG',
-    function (event, param1, param2) {
+$(document).on('load_xmodule', 'a.modal-content img', function () {
+    'use strict';
 
+    var body = $('body');
+    var wrapper = replaceHtml(this);
+    var imageWrapper = wrapper.find('.image-wrapper');
+    var image = imageWrapper.find('img');
+    var imageModal = wrapper.find('.image-modal');
+    var imageLink = wrapper.find('.image-link');
+    var imageContent = imageModal.find('.image-content');
+    var imageControls = imageContent.find('.image-controls');
+    var actionClose = imageContent.find('.action-close');
+    var actionZoomIn = imageControls.find('.action-zoom-in');
+    var actionZoomOut = imageControls.find('.action-zoom-out');
+    var modalUiIcon = imageControls.find('.modal-ui-icon');
+    var draggie = new Draggabilly(image[0], {
+        containment: true
+    });
 
-  $("a.modal-content").each(function() {
-    var smallImageObject = $(this).children();
-    var largeImageSRC = $(this).attr('href');
-    
-    // if contents of zoomable link is image and large image link exists: setup modal
-    if (smallImageObject.is('img') && largeImageSRC) {
-      var data = {
-        "smallHTML": $(this).html(),
-        "largeALT": smallImageObject.attr('alt'),
-        "largeSRC": largeImageSRC
-      };
-      var html = _.template($("#image-modal-tpl").text(), data);
-      $(this).replaceWith(html);
+    function attachEvents() {
+        draggie.disable();
+        imageLink.click(openModal);
+        imageModal.click(hideModal);
+        actionClose.click(hideModal);
+        image.click(preventEventBubbling);
+        imageControls.click(preventEventBubbling);
+        actionZoomOut.click(toggleZoom);
+        actionZoomIn.click(toggleZoom);
     }
-  });
-  $('.wrapper-modal-image .image-wrapper img').each(function() {
-    var draggie = new Draggabilly(this, {containment: true});
-    draggie.disable();
-    $(this).closest('.image-modal').data("draggie", draggie);
-  });
-
-  // Opening and closing image modal on clicks
-  $(".wrapper-modal-image .image-link").click(function() {
-    $(this).siblings(".image-modal").addClass('image-is-fit-to-screen');
-    $('body').css('overflow', 'hidden');
-  });
-  
-  // variable to detect when modal is being "hovered".
-  // Done this way as jquery doesn't support the :hover psudo-selector as expected.
-  var imageModalImageHover = false;
-  $(".wrapper-modal-image .image-content img, .wrapper-modal-image .image-content .image-controls").hover(function() {
-    imageModalImageHover = true;
-  }, function() {
-    imageModalImageHover = false;
-  });
-  
-  // prevent image control button links from scrolling
-  $(".modal-ui-icon").click(function(event) {
-    event.preventDefault();
-  });
-  
-  //Define function to close modal
-  function closeModal(imageModal) {
-    imageModal.removeClass('image-is-fit-to-screen').removeClass('image-is-zoomed');
-    $(".wrapper-modal-image .image-content .image-controls .modal-ui-icon.action-zoom-in").removeClass('is-disabled');
-    $(".wrapper-modal-image .image-content .image-controls .modal-ui-icon.action-zoom-out").addClass('is-disabled');
-    var currentDraggie = imageModal.data("draggie");
-    currentDraggie.disable();
-    $('body').css('overflow', 'auto');
-  }
-  
-  // Click outside of modal to close it.
-  $(".wrapper-modal-image .image-modal").click(function() {
-    if (!imageModalImageHover){
-      closeModal($(this));
+    function preventEventBubbling() {
+        return false;
     }
-  });
-  
-  // Click close icon to close modal.
-  $(".wrapper-modal-image .image-content .action-remove").click(function() {
-    closeModal($(this).closest(".image-modal"));
-  });
-
-  // zooming image in modal and allow it to be dragged
-  // Make sure it always starts zero position for below calcs to work
-  $(".wrapper-modal-image .image-content .image-controls .modal-ui-icon").click(function() {
-    if (!$(this).hasClass('is-disabled')) {
-      var mask = $(this).closest(".image-content");
-      
-      var imageModal = $(this).closest(".image-modal");
-      var img = imageModal.find("img");
-      var currentDraggie = imageModal.data("draggie");
-      
-      if ($(this).hasClass('action-zoom-in')) {
-        imageModal.removeClass('image-is-fit-to-screen').addClass('image-is-zoomed');
-        
-        var imgWidth   = img.width();
-        var imgHeight  = img.height();
-        
-        var imgContainerOffsetLeft = imgWidth - mask.width();
-        var imgContainerOffsetTop = imgHeight - mask.height();
+    function openModal() {
+        imageModal.addClass('image-is-fit-to-screen');
+        body.css('overflow', 'hidden');
+        return false;
+    }
+    function hideModal() {
+        imageModal
+            .removeClass('image-is-fit-to-screen')
+            .removeClass('image-is-zoomed')
+        ;
+        actionZoomIn.removeClass('is-disabled');
+        actionZoomOut.addClass('is-disabled');
+        draggie.disable();
+        body.css('overflow', 'auto');
+        return false;
+    }
+    function toggleZoom(event) {
+        if (!$(event.target).parent().hasClass('is-disabled')) {
+            modalUiIcon.toggleClass('is-disabled');
+            imageModal
+                .toggleClass('image-is-fit-to-screen')
+                .toggleClass('image-is-zoomed')
+            ;
+            if (draggie.isEnabled) {
+                draggie.disable();
+            } else {
+                draggie.enable();
+                centerImage();
+            }
+        }
+        return false;
+    }
+    function centerImage() {
+        var imgWidth = image.width();
+        var imgHeight = image.height();
+        var imgContainerOffsetLeft = imgWidth - imageContent.width();
+        var imgContainerOffsetTop = imgHeight - imageContent.height();
         var imgContainerWidth = imgWidth + imgContainerOffsetLeft;
         var imgContainerHeight = imgHeight + imgContainerOffsetTop;
-        
-        // Set the width and height of the image's container so that the dimensions are equal to the image dimensions + view area dimensions to limit dragging
-        // Set image container top and left to center image at load.
-        img.parent().css({
-          left: -imgContainerOffsetLeft,
-          top: -imgContainerOffsetTop,
-          width: imgContainerWidth,
-          height: imgContainerHeight
+        image.css({
+            top: imgContainerOffsetTop / 2,
+            left: imgContainerOffsetLeft / 2
         });
-        img.css({top: imgContainerOffsetTop / 2, left: imgContainerOffsetLeft / 2});
-        
-        currentDraggie.enable();
-        
-      } else if ($(this).hasClass('action-zoom-out')) {
-        imageModal.removeClass('image-is-zoomed').addClass('image-is-fit-to-screen');
-        
-        currentDraggie.disable();
-      }
-      
-      $(".wrapper-modal-image .image-content .image-controls .modal-ui-icon").toggleClass('is-disabled');
+        imageWrapper.css({
+            top: -imgContainerOffsetTop,
+            left: -imgContainerOffsetLeft,
+            width: imgContainerWidth,
+            height: imgContainerHeight
+        });
     }
-  });
-}
+    function replaceHtml(that) {
+        var data;
+        var html;
+        var image = $(that);
+        var image_alt = image.attr('alt');
+        var anchor = image.parent();
+        var anchor_href = anchor.attr('href');
+        var anchor_html = anchor.html();
+        var template = $('#image-modal-tpl');
+        if (!anchor_href) {
+            return;
+        }
+        data = {
+            "smallHtml": anchor_html,
+            "largeAlt": image_alt,
+            "largeSrc": anchor_href
+        };
+        html = _.template(template.text(), data);
+        wrapper = $(html);
+        anchor.replaceWith(wrapper);
+        return wrapper;
+    }
 
+    attachEvents();
+});
 
-);
